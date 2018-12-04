3a27c35f1fdc81d31963f3e51f292a29f9722ad0
Ephraim Berkovitch <ephraim.berkovitch@gmail.com>
2018-12-04 17:50:26 +0200

    Init venv and 2 first functions

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+if __name__ == '__main__':
+    print(add(3,5))
